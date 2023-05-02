abc43cee875c512d1c379b7deda9302a8c638415
Bohdan <bohdankorchevskiy@gmail.com>
2023-05-02 13:51:36 +0300

    Added base structure

---
+++ be-spring-security/src/main/java/com/spring/security/backend/BeSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.spring.security.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSpringSecurityApplication.class, args);
+	}
+
+}
